b9253506bb97aa073132a0401570d9528067c0e7
Dmitriy Dihtiarov <dmitriydegtyarev1985@gmail.com>
2021-05-04 22:56:32 +0300

    change function on markup

--- src/js/markup.js
+++ src/js/markup.js
@@ -2,19 +2,10 @@ import cardsMenu from '../menu.json'
 import templates from '../templates/cardsDishes.hbs';
 import { refs } from '../js/refs';
 
-// const createMarkup = () => templates(cardsMenu);
+const createMarkup = () =>
+  templates(cardsMenu);
 
-// const { menuItem } = refs;
-
-function createMarkup() {
-  return templates(cardsMenu);
-}
-
-// const renderMarkup = () => refs.menuItem.inserrtAdjacentHTML('beforeend', createMarkup());
-
-function renderMarkup() {
-  const markup = createMarkup();
-  refs.menuItem.insertAdjacentHTML('beforeend', markup);
-}
+const renderMarkup = () =>
+  refs.menuItem.insertAdjacentHTML('beforeend', createMarkup());
 
 renderMarkup();
