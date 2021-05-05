a81b246be72eac097a6fbe42eb3bf0d4d39790ba
Dmitriy Dihtiarov <dmitriydegtyarev1985@gmail.com>
2021-05-05 23:52:02 +0300

    finish switch theme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import './css/styles.css';
-// import cardsMenu from './menu.json'
-// import templates from './templates/cardsDishes.hbs';
-// import { Theme, localStorageThemeKey } from './js/const';
-// import { refs } from './js/refs';
 import './js/markup';
-
-console.log(1);
-
+import './js/theme';
---
+++ src/js/theme.js
@@ -0,0 +1,30 @@
+import { refs } from '../js/refs';
+import { localStorageThemeKey, Theme } from '../js/const';
+
+refs.themeSwitchButton.addEventListener('change', event => switchTheme(event));
+
+function switchTheme({ target }) {
+  
+  target.checked
+    ? refs.body.classList.replace(Theme.LIGHT, Theme.DARK)
+    : refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+  
+  const theme = target.checked ? Theme.DARK : Theme.LIGHT;
+
+  localStorage.setItem(localStorageThemeKey, theme);
+}
+
+function getCurrentTheme() {
+  const selectedTheme = localStorage.getItem(localStorageThemeKey);
+  if (selectedTheme) {
+    refs.themeSwitchButton.checked = selectedTheme === Theme.DARK;
+  }
+  return selectedTheme ? selectedTheme : Theme.LIGHT;
+}
+
+function initTheme() {
+  const theme = getCurrentTheme();
+  refs.body.classList.add(theme);
+}
+
+initTheme();
